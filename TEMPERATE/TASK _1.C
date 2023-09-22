d71730c07f47ad8713d832a48a476965188cf843
Smit Chitroda <143987366+chitrodaS@users.noreply.github.com>
2023-09-22 10:14:35 +0530

    Add files via upload

---
+++ TEMPERATE/TASK _1.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#define P printf
+#define S scanf
+void main()
+{
+	char alfabet= 'a';
+	clrscr();
+	do
+	{
+		P("%c ",alfabet);
+	       alfabet+=4;
+	}while(alfabet<='z')
+
+	getch();
+}
+
