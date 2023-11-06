9cadf70e46361fd2846ccc9b8201e5f9fed2070a
Smit Chitroda <143987366+chitrodaS@users.noreply.github.com>
2023-11-06 09:30:14 +0530

    Update 2.c

--- CH 10/TSRS/2.c
+++ CH 10/TSRS/2.c
@@ -1,1 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
+
+int stringLength(char str[])
+{
+    int length = 0;
+
+    for (int i = 0; str[length] != '\0'; i++)
+    {
+        length++;
+    }
+
+    return length;
+}
+
+int main()
+{
+    char string[20];
+
+    printf("Enter any string: ");
+    scanf("%[^\n]", string);
+
+    printf("Length is: %d\n", stringLength(string));
+
+    return 0;
+}
