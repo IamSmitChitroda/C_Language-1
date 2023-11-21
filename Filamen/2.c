ebd99623ff1413bccbf633d89d50fa5a02b510da
Smit Chitroda <143987366+chitrodaS@users.noreply.github.com>
2023-11-21 10:45:29 +0530

    Update 2.c

--- Filamen/2.c
+++ Filamen/2.c
@@ -7,17 +7,25 @@ int main()
 
     int len = 0, count;
 
-    for (int i = 0; string != '\0'; i++)
+    for (int i = 0; string[i] != '\0'; i++)
         len++;
+
     for (int i = 0; i < len; i++)
     {
         count = 1;
-        for (int j = i + 1; j < len; j++)
-            if (string[i] == string[j])
-                count++;
-        string[i] = NULL;
+        if (string[i] != '\0')
+        {
+            for (int j = i + 1; j < len; j++)
+            {
 
-        printf("%s ===> %d", string[i], count);
+                if (string[i] == string[j])
+                {
+                    count++;
+                    string[j] = '\0';
+                }
+            }
+            printf("%c ===> %d\n", string[i], count);
+        }
     }
 
     return 0;
