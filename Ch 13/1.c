2bebb2f7a4f0653ce415494c8d8dc4f2bc85358f
Smit Chitroda <143987366+chitrodaS@users.noreply.github.com>
2023-12-02 12:09:02 +0530

    Update 1.c

--- Ch 13/1.c
+++ Ch 13/1.c
@@ -1,11 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    FILE *read;
+    FILE *read, *write;
+    char data[50];
 
-    read = fopen("1st.txt" , "r");
+    read = fopen("1st.txt", "r");
+    write = fopen("2nd.txt", "w");
 
-    
+    if (read != NULL)
+    {
+        while (fgets(data, 2, read) != NULL)
+        {
+            printf("%s", data);
+            fprintf(write, "%s", data);
+        }
+    }
+
     return 0;
 }
