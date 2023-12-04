bce3877eab2ca45d66227d7cdd86dbf2b342c422
Smit Chitroda <143987366+chitrodaS@users.noreply.github.com>
2023-12-04 21:31:52 +0530

    Update 3.c

--- Exam/3.c
+++ Exam/3.c
@@ -1,48 +1,59 @@
 #include <stdio.h>
-#define p printf
-#define s scanf
 
-struct pro
+struct Product
 {
-    char pname[20];
-    int pq, pno, pp;
+    int productNo;
+    char productName[50];
+    float price;
+    int quantity;
 };
 
-main()
+float calculateTotal(struct Product p)
 {
+    float total = p.price * p.quantity;
+    return total + (total * 0.18);
+}
 
-    FILE *fp;
+int main()
+{
+    FILE *filePointer;
+    filePointer = fopen("bill.txt", "w");
 
-    fp = fopen("bill.txt", "w");
+    int N, i;
+    char customerName[50], contactNo[15];
+    printf("Enter the number of products: ");
+    scanf("%d", &N);
 
-    char name[20], No[10];
-    int product, total = 0, i;
+    struct Product products[N];
 
-    p("Enter your name : ");
-    s("%[^\n]", &name);
-    p("Enter your contect no. : ");
-    s("%s", &No);
-    fprintf(fp, "%s", name);
-    fprintf(fp, "%s", No);
+    printf("Enter customer name: ");
+    scanf("%s", customerName);
 
-    p("\n\nEnter your product : ");
-    s("%d", &product);
-    struct pro ob[product];
+    printf("Enter contact number: ");
+    scanf("%s", contactNo);
 
-    for (i = 0; i < product; i++)
+    fprintf(filePointer, "Customer Name: %s\nContact Number: %s\n\n", customerName, contactNo);
+    fprintf(filePointer, "Product No\tProduct Name\tPrice\tQuantity\tTotal\n");
+    printf("Product No\tProduct Name\tPrice\tQuantity\tTotal\n");
+
+    for (i = 0; i < N; i++)
     {
-        p("Enter product name : ");
-        s("%s", &ob[i].pname);
-        p("Enter product no : ");
-        s("%s", &ob[i].pno);
-        p("Enter product Quenty : ");
-        s("%d", &ob[i].pq);
-        p("Enter product price : ");
-        s("%s", &ob[i].pp);
+        printf("Enter details for Product %d:\n", i + 1);
+        printf("Product Number: ");
+        scanf("%d", &products[i].productNo);
+        printf("Product Name: ");
+        scanf("%s", products[i].productName);
+        printf("Price: ");
+        scanf("%f", &products[i].price);
+        printf("Quantity: ");
+        scanf("%d", &products[i].quantity);
+
+        float total = calculateTotal(products[i]);
+
+        fprintf(filePointer, "%d\t\t%s\t\t%.2f\t%d\t\t%.2f\n", products[i].productNo, products[i].productName, products[i].price, products[i].quantity, total);
     }
 
-    for (i = 0; i < product; i++)
-        total = total + (ob[i].pp + ob[i].pq);
-
-    printf("Bill : %d", (total) + (total * 0.18));
+    fclose(filePointer);
+    printf("Bill generated and stored in 'bill.txt' file.\n");
+    return 0;
 }
